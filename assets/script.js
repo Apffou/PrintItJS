bda45c6e000f873ded515a62f290e86a5a49a70c
Anaïs Zafiriou-Lejeune <anais.ljn@outlook.fr>
2023-10-06 12:18:15 +0200

    Installation des flèches dans le html + commentaires pour etape dans JS

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,21 @@
+/** Souhait client : 
+ * Boucle à l'infini 
+ * Dots à chaque image / dot rempli sur la bonne image. ex : img2 = dot 2 rempli
+ * flèche sur le coté
+ * 
+ ** Demarche
+ * Utilisation d'event listener click sur chaque fleche
+ * Au click on passe à l'autre image, le dot change également
+ * Si on est à la dernière image et que l’on clique à droite : 
+on affiche la première image ;
+le point sélectionné est le premier.
+Si on est à la première image et qu’on clique à gauche : 
+on affiche la dernière image ;
+le point sélectionné est le dernier. 
+Dans tous les cas, le texte change en accord avec l’image montrée.
+ */
+
+
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -16,3 +34,5 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
